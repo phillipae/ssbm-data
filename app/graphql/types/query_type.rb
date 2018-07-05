a984fb1f0f7f21a56f6743583c1d397f7853054f
Phil <pespinoza@missiondata.com>
2018-07-05 18:57:08 -0400

    [non-working, record-keeping] note initial approach to graphql implementation

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,8 +2,10 @@
 Types::QueryType = GraphQL::ObjectType.define do
   # Add root-level fields here.
   # They will be entry points for queries on your schema.
+  name "Query"
+  description "The query root for this schema"
 
-  field :allCharacters, !types[Types::CharacterType] do
+  field :characters, !types[Types::CharacterType] do
     resolve -> (obj, args, ctx) { Character.all }
   end
 
