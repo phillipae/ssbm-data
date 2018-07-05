eb3bd22c4c1f88d6fc72e3dd837a66d96a97c552
Phil <pespinoza@missiondata.com>
2018-07-05 19:25:14 -0400

    Functional GraphiQL setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # if Rails.env.development?
-  #   mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  # end
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/query"
+  end
   post "/query", to: "application#query"
 end
