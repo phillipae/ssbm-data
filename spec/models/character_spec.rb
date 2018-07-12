7852b107ff4a9cf83613fde6664d1a6e0c3f0456
Phil <pespinoza@missiondata.com>
2018-07-12 19:02:15 -0400

    Streamline character attribute validity checking in spec

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper.rb'
 
+def check_attribute_value_within_range(attr_name, attr_min_value, attr_max_value)
+  it "has a(n) #{attr_name} value within valid range" do
+    expect(char.send(attr_name.to_sym)).to be_between(attr_min_value, attr_max_value)
+  end      
+end
+
 describe Character do
   it "has a valid factory" do
   end
@@ -19,50 +25,20 @@ describe Character do
   context "with valid attributes" do
     let(:char) { FactoryBot.create :character }
 
-    it "returns a title" do
-      expect(char.title).to be_a(String)
-    end
-
-    it "returns a rank" do
-      expect(char.rank).to be_between(1, 30)
-    end
-
-    it "returns an air_acceleration_base" do
-      expect(char.air_acceleration_base).to be_between(0.01, 0.19)
-    end
-
-    it "returns an air_acceleration_additional" do
-      expect(char.air_acceleration_additional).to be_between(0.0125, 0.09)
-    end
-
-    it "returns an air_acceleration_max" do
-      expect(char.air_acceleration_max).to be_between(0.0325, 0.28)
-    end
-
-    it "returns an air_friction" do
-      expect(char.air_friction).to be_between(0.001, 0.05)
-    end
-
-    it "returns an air_speed" do
-      expect(char.air_speed).to be_between(0.68, 1.35)
-    end
-
-    it "returns a fall_speed_base" do
-      expect(char.fall_speed_base).to be_between(1.3, 3.1)
-    end
-
-    it "returns a fall_speed_fast" do
-      expect(char.fall_speed_fast).to be_between(1.6, 3.5)
-    end
-
-    it "returns a gravity" do
-      expect(char.gravity).to be_between(0.064, 0.25)
-    end
-
-    it "returns a weight" do
-      expect(char.weight).to be_between(55, 140)
+    [
+      ["rank",                        1,      30],
+      ["air_acceleration_base",       0.01,   0.19],
+      ["air_acceleration_additional", 0.0125, 0.09],
+      ["air_acceleration_max",        0.0325, 0.28],
+      ["air_friction",                0.001,  0.05],
+      ["air_speed",                   0.68,   1.35],
+      ["fall_speed_base",             1.3,    3.1],
+      ["fall_speed_fast",             1.6,    3.5],
+      ["gravity",                     0.064,  0.25],
+      ["weight",                      55,     140]
+    ].each do |attr_name, attr_min_value, attr_max_value|
+      check_attribute_value_within_range(attr_name, attr_min_value, attr_max_value)
     end
 
   end
-
 end
