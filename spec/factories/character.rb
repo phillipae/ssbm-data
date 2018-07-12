65288806121446cb8adc46e159aea70109d7b42c
Phil <pespinoza@missiondata.com>
2018-07-12 18:34:17 -0400

    Add specs for character numeric attributes

--- spec/factories/character.rb
+++ spec/factories/character.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     rank                        { Faker::Number.between(1, 30) }
     air_acceleration_base       { Faker::Number.between(0.01, 0.19).truncate(2) }
     air_acceleration_additional { Faker::Number.between(0.0125, 0.09).truncate(4) }
-    air_acceleration_max        { Faker::Number.between(0.28, 0.0325).truncate(4) }
+    air_acceleration_max        { Faker::Number.between(0.0325, 0.28).truncate(4) }
     air_friction                { Faker::Number.between(0.001, 0.05).truncate(3) }
     air_speed                   { Faker::Number.between(0.68, 1.35).truncate(2) }
     fall_speed_base             { Faker::Number.between(1.3, 3.1).truncate(2) }
--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -1,9 +1,68 @@
 require 'spec_helper.rb'
 
 describe Character do
-  it "has a valid factory"
-  it "is invalid without a title"
-  it "is invalid without a rank"
-  it "returns a title"
-  it "returns a rank"
+  it "has a valid factory" do
+  end
+
+  context "without valid attributes" do
+      it "is invalid without a title" do
+        char = Character.new(title: nil)
+        expect(char).not_to be_valid
+      end
+
+      it "is invalid without a rank" do
+        char = Character.new(rank: nil)
+        expect(char).not_to be_valid
+      end
+  end
+
+  context "with valid attributes" do
+    let(:char) { FactoryBot.create :character }
+
+    it "returns a title" do
+      expect(char).to receive(:title)
+    end
+
+    it "returns a rank" do
+      expect(char.rank).to receive(:rank)
+    end
+
+    it "returns an air_acceleration_base" do
+      expect(char.air_acceleration_base).to be_between(0.01, 0.19)
+    end
+
+    it "returns an air_acceleration_additional" do
+      expect(char.air_acceleration_additional).to be_between(0.0125, 0.09)
+    end
+
+    it "returns an air_acceleration_max" do
+      expect(char.air_acceleration_max).to be_between(0.0325, 0.28)
+    end
+
+    it "returns an air_friction" do
+      expect(char.air_friction).to be_between(0.001, 0.05)
+    end
+
+    it "returns an air_speed" do
+      expect(char.air_speed).to be_between(0.68, 1.35)
+    end
+
+    it "returns a fall_speed_base" do
+      expect(char.fall_speed_base).to be_between(1.3, 3.1)
+    end
+
+    it "returns a fall_speed_fast" do
+      expect(char.fall_speed_fast).to be_between(1.6, 3.5)
+    end
+
+    it "returns a gravity" do
+      expect(char.gravity).to be_between(0.064, 0.25)
+    end
+
+    it "returns a weight" do
+      expect(char.weight).to be_between(55, 140)
+    end
+
+  end
+
 end
