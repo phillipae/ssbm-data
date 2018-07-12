ade17cade6131fb70660e1a1516cd6d4a9f11f0b
Phil <pespinoza@missiondata.com>
2018-07-12 19:56:38 -0400

    Streamline char spec testing data points

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -1,5 +1,40 @@
 require 'spec_helper.rb'
 
+def attr_names
+  Character.column_names
+end
+
+def attr_numerics_names
+  attr_names - %w[id title created_at updated_at]
+end
+
+def attr_ranges
+  [
+    [1,      30],
+    [0.01,   0.19],
+    [0.0125, 0.09],
+    [0.0325, 0.28],
+    [0.001,  0.05],
+    [0.68,   1.35],
+    [1.3,    3.1],
+    [1.6,    3.5],
+    [0.064,  0.25],
+    [55,     140]
+  ]
+end
+
+def attr_numerics_with_ranges
+  attr_numerics_names.zip(attr_ranges).map(&:flatten)
+end
+
+def check_attribute_value_exists(attr_name)
+  it "is invalid without a #{attr_name}" do
+    char = Character.new
+    char.send("#{attr_name}=", nil)
+    expect(char).not_to be_valid
+  end
+end
+
 def check_attribute_value_within_range(attr_name, attr_min_value, attr_max_value)
   it "has a(n) #{attr_name} value within valid range" do
     expect(char.send(attr_name.to_sym)).to be_between(attr_min_value, attr_max_value)
@@ -11,32 +46,15 @@ describe Character do
   end
 
   context "without valid attributes" do
-      it "is invalid without a title" do
-        char = Character.new(title: nil)
-        expect(char).not_to be_valid
-      end
-
-      it "is invalid without a rank" do
-        char = Character.new(rank: nil)
-        expect(char).not_to be_valid
-      end
+    attr_names.each do |attr_name|
+      check_attribute_value_exists(attr_name)
+    end
   end
 
   context "with valid attributes" do
     let(:char) { FactoryBot.create :character }
 
-    [
-      ["rank",                        1,      30],
-      ["air_acceleration_base",       0.01,   0.19],
-      ["air_acceleration_additional", 0.0125, 0.09],
-      ["air_acceleration_max",        0.0325, 0.28],
-      ["air_friction",                0.001,  0.05],
-      ["air_speed",                   0.68,   1.35],
-      ["fall_speed_base",             1.3,    3.1],
-      ["fall_speed_fast",             1.6,    3.5],
-      ["gravity",                     0.064,  0.25],
-      ["weight",                      55,     140]
-    ].each do |attr_name, attr_min_value, attr_max_value|
+    attr_numerics_with_ranges.each do |attr_name, attr_min_value, attr_max_value|
       check_attribute_value_within_range(attr_name, attr_min_value, attr_max_value)
     end
 
